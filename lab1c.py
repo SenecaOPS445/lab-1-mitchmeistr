cdee68b02f4ab5b28d78f0dd522f1cccb43fa888
mitch <mgregoris2@myseneca.ca>
2024-05-14 22:48:27 -0400

    integer object investigation complete

---
+++ lab1c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Author: Mitchell Gregoris
+Student ID: 133349191
+Email: mgregoris2@myseneca.ca
+Program Name: lab1a.py
+Date: 2024-05-14
+Description: Integer Objects
+'''
+name = 'Isaac'
+age = 72
+print(name + ' is ' + str(age) + ' years old!')
