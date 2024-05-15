d6c7707f8b9bb3470ebdbe135d51ec6c0eb60e58
mitch <mgregoris2@myseneca.ca>
2024-05-14 22:31:15 -0400

    string objects

---
+++ lab1b.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Author: Mitchell Gregoris
+Student ID: 133349191
+Email: mgregoris2@myseneca.ca
+Program Name: lab1a.py
+Date: 2024-05-14
+Description: 
+'''
+name = 'Isaac'
+print('How old are you ' + name + '?')
